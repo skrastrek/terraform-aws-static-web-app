208bf2aa19d2909555c8728951c63afa9c627756
SebRamsland <mail@sebastianramsland.com>
2026-02-02 10:31:59 +0100

    chore: Use Skrastrek Terraform Registry

--- iam.tf
+++ iam.tf
@@ -4,7 +4,8 @@ resource "aws_iam_policy" "s3_bucket_read" {
 }
 
 module "s3_bucket_read" {
-  source = "github.com/skrastrek/terraform-modules-aws-iam//policy-document/s3-bucket-read?ref=v0.0.1"
+  source  = "skrastrek/iam/aws//modules/policy-document/s3-bucket-read"
+  version = "1.0.0"
 
   s3_bucket_arn = aws_s3_bucket.this.arn
 }
@@ -15,7 +16,8 @@ resource "aws_iam_policy" "s3_bucket_write" {
 }
 
 module "s3_bucket_write" {
-  source = "github.com/skrastrek/terraform-modules-aws-iam//policy-document/s3-bucket-write?ref=v0.0.1"
+  source  = "skrastrek/iam/aws//modules/policy-document/s3-bucket-write"
+  version = "1.0.0"
 
   s3_bucket_arn = aws_s3_bucket.this.arn
 }
